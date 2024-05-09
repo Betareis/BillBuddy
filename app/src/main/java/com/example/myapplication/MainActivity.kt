c13fb09f452567e60730f41970e4afce74243e8a
Betareis <105857372+Betareis@users.noreply.github.com>
2024-05-09 17:47:43 +0200

    start code
    added MainActivity
    BillBuddyApp
    HomePage

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication
 
+// Importieren der benötigten Klassen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //dieser code ist zu 99% fertig
+
+        val textView = TextView(this)
+        textView.text = "Hello World!"
+        textView.textSize = 24f
+        setContentView(textView)
     }
 }
+
+
+
