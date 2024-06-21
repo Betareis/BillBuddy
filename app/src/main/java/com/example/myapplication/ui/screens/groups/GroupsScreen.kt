b49e8fc34901b7775357f3a1cb3e2f94e82c6d83
Betareis <beatrice.bley@t-online.de>
2024-06-21 15:31:29 +0200

    Added title "Groups"

--- app/src/main/java/com/example/myapplication/ui/screens/groups/GroupsScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/groups/GroupsScreen.kt
@@ -54,6 +54,7 @@ import kotlinx.coroutines.launch
 import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.ui.draw.clip
 import androidx.lifecycle.liveData
@@ -72,9 +73,12 @@ fun GroupsScreen(navController: NavController, groupViewModel: GroupsViewModel =
 
     Scaffold(
         contentColor = Color.Black,
-        bottomBar = { TabView(navController) }
-
-
+        bottomBar = { TabView(navController) },
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Groups") }
+            )
+        }
     ) {
         Surface(
             modifier = Modifier
@@ -116,7 +120,8 @@ fun GroupsScreen(navController: NavController, groupViewModel: GroupsViewModel =
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Add,
-                        contentDescription = "Add a Group" // Provide a description for accessibility
+                        contentDescription = "Add a Group", // Provide a description for accessibility
+                        tint = Color.Black
                     )
                 }
             }
