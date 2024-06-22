8d3d3302f6c6cd09ed86deeaadf12ddbc6681ec1
Betareis <beatrice.bley@t-online.de>
2024-06-22 10:48:16 +0200

    kommentare für implementierung

--- app/src/main/java/com/example/myapplication/domain/balanceManagement/CalculateBalancesUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/balanceManagement/CalculateBalancesUseCase.kt
@@ -4,5 +4,8 @@ import com.example.myapplication.data.repository.FirestoreRepository
 import javax.inject.Inject
 
 class CalculateBalancesUseCase @Inject constructor(private val firestoreRepository: FirestoreRepository) {
-
+    // Transactions aus jeweiliger Gruppe bekommen
+    // Abfragen wer welche Transaction bezahlt hat (payedBy)
+    // Abfragen wer, mit welchem single amount an der transaction beteiligt war
+    // Für jeden Nutzer der Gruppe ausrechnen, wie seine eigene Balance nach allen transaktionen in der gruppe ist
 }
