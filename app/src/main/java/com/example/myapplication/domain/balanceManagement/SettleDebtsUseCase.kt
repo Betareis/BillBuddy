05570120d3d89359f7cb2016884447bcbba5a863
Ramon Bendinger <65312605+Dohani@users.noreply.github.com>
2024-06-26 18:37:03 +0200

    Deletion: Useless files

--- app/src/main/java/com/example/myapplication/domain/balanceManagement/SettleDebtsUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.domain.balanceManagement
-
-import com.example.myapplication.data.repository.FirestoreRepository
-import javax.inject.Inject
-
-class SettleDebtsUseCase @Inject constructor(private val firestoreRepository: FirestoreRepository) {
-}
--- app/src/main/java/com/example/myapplication/domain/transactionManagement/EditTransactionUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.domain.transactionManagement
-
-import com.example.myapplication.data.repository.FirestoreRepository
-import javax.inject.Inject
-
-class EditTransactionUseCase @Inject constructor(private val firestoreRepository: FirestoreRepository) {
-}
